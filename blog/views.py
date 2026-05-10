ed79fc36a907643ffe61293a646d3d98c81dd4f6
Finnegan <finneganadair@gmail.com>
2026-05-10 11:40:22 -0400

    fixing views

--- blog/views.py
+++ blog/views.py
@@ -26,3 +26,21 @@ def login_view(req):
    redirect_uri = os.getenv("URI")
    ion_auth_url = f"https://ion.tjhsst.edu/oauth/authorize?response_type=code&client_id={client_id}&redirect_uri={redirect_uri}&scope=read+openid"
    return redirect(ion_auth_url)
+def callback_viwe(req):
+   code = req.get.GET("code")
+   id = os.getenv("ION_CLIENT_SECRET")
+   response = requests.post("https://ion.tjhsst.edu/oauth/token/", data={
+   "grant_type":"authorization_code",
+   "code":code,
+   "client_id":id,
+   "client_secret": secret,
+   "redirect_uri" = os.getenv("URI")
+   })
+   data = response.json
+   access_token = data.get("access_token")
+   profile_response = requests.get("https://ion.tjhsst.edu/api/profile", headers={"Authorization:" f"Bearer {access_token}")
+   username = profile_data.get("ion_username")
+   user, created = User.objects.get_or_create(username=username)
+   profile, profile_created = Profile.objects.get_or_create(user=user)
+   login(req, user)
+   return redirect("/")
